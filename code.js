30871590e33505959445cbe8f79aa1c484e75ae8
Marwan Selim <marwanselim306@gmail.com>
2023-03-22 14:54:15 +0200

    final look was added

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ let onlinePic = new Array();
 let name = new Array();
 let price = new Array();
 let pic = new Array();
+let cart = new Array();
 
 
 
@@ -11,10 +12,53 @@ let sum=0
 
 function addItem(index){
     sum = sum + +price[index]
-    let li= document.createElement('li')
-    li.innerText=name[index]+"\ncost:"+price[index]
-    document.getElementById('side_list').appendChild(li)
-    document.getElementById('side_total').innerText='total = '+sum+'$'
+    cart.push(name[index]+"\ncost:"+price[index])
+    console.log(cart)
+    
+}
+function buildCart(){
+    let dev = document.createElement('div')
+    dev.className='shoping_cart_container'
+    let dev2 = document.createElement('div')
+    dev2.className = "shoping_cart"
+    let text = document.createElement('h1')
+    text.innerText = 'Total'
+    dev2.appendChild(text)
+    let list = document.createElement('ul')
+    list.setAttribute('id',"side_list")
+    for(let i = 0; i<cart.length ; i++){
+        let l = document.createElement('li')
+        l.innerText = cart[i]
+        list.appendChild(l)
+    }
+    dev2.appendChild(list)
+    text = document.createElement('h3')
+    text.innerText = 'Total = '+sum
+    text.setAttribute('id',"side_total")
+    dev2.appendChild(text)
+    text = document.createElement('h2')
+    text.innerText = 'address'
+    dev2.appendChild(text)
+    let input = document.createElement('input')
+    input.setAttribute('type',"text")
+    let button = document.createElement('button')
+    button.setAttribute('type','button')
+    button.innerText='place order'
+    button.addEventListener('click',order)
+    let img = document.createElement('img')
+    img.className='background-img'
+    img.setAttribute('src', "img\\Asset 1@3x-8.png")
+    dev2.appendChild(input)
+    dev2.appendChild(button)
+    dev.appendChild(dev2)
+    dev.appendChild(img)
+    const main = document.getElementById('main')
+    while (main.hasChildNodes()) {
+        main.removeChild(main.firstChild);
+      }
+    main.appendChild(dev)
+
+
 }
 
 function order(){
@@ -41,7 +85,14 @@ async function orderToys(title){
         dev.className='item'
         let txt = document.createElement('h1')
         txt.className = 'item_text'
-        txt.innerText= name[i]+'\n cost ='+price[i]
+        txt.innerText= name[i]
+        let txt2 = document.createElement('h2')
+        txt2.className='item_price'
+        txt2.innerText = 'cost = '+price[i]
+        let dev2 = document.createElement('div')
+        dev2.className='text_holder'
+        let dev3 = document.createElement('div')
+        dev3.className='button_holder'
         let button = document.createElement('button')
         button.setAttribute('type','button')
         button.className='item_but'
@@ -52,9 +103,12 @@ async function orderToys(title){
         let img = document.createElement('img')
         img.className='item_img'
         img.setAttribute('src', 'img\\'+title+pic[i]+'.jpg')
+        dev2.appendChild(txt)
+        dev2.appendChild(txt2)
+        dev3.appendChild(button)
         dev.appendChild(img)
-        dev.appendChild(txt)
-        dev.appendChild(button)
+        dev.appendChild(dev2)
+        dev.appendChild(dev3)
         main.appendChild(dev)
     }
 }
@@ -70,6 +124,10 @@ async function online(){
         let txt = document.createElement('h1')
         txt.className = 'item_text'
         txt.innerText= onlineName[i]
+        let dev2 = document.createElement('div')
+        dev2.className='text_holder'
+        let dev3 = document.createElement('div')
+        dev3.className='button_holder'
         let button = document.createElement('a')
         button.setAttribute('href',onlineLink[i])
         button.className='item_but'
@@ -77,9 +135,11 @@ async function online(){
         let img = document.createElement('img')
         img.className='item_img'
         img.setAttribute('src', 'img\\online'+onlinePic[i]+'.jpg')
+        dev2.appendChild(txt)
+        dev3.appendChild(button)
         dev.appendChild(img)
-        dev.appendChild(txt)
-        dev.appendChild(button)
+        dev.appendChild(dev2)
+        dev.appendChild(dev3)
         main.appendChild(dev)
     }
 
@@ -115,7 +175,8 @@ document.getElementById('children').addEventListener('click',()=>{orderToys('chi
 document.getElementById('teens').addEventListener('click',()=>{orderToys('teen')})
 document.getElementById('open').addEventListener('click',()=>{orderToys('open')})
 document.getElementById('online').addEventListener('click',online)
-document.getElementById("side_but").addEventListener('click',order)
+document.getElementById('cart').addEventListener('click',buildCart)
+
 
 
 
